a1be94668141753cc84f3ab79275b71cfbbc3a55
balaji.arulkumar.dev <balaji.arulkumar.dev@gmail.com>
2026-08-25 14:17:11 +0530

    Code formatter added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-  return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <h1 className="text-2xl font-semibold tracking-tight">Dashboard</h1>
-      <p className="text-sm text-muted-foreground">
-        Under construction.
-      </p>
-    </div>
-  )
+  return redirect("/customers")
 }
